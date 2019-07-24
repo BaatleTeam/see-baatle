1bd62c7c9b3282102cb7a35b26fc56ff75b5095e
makometr <makometr@yandex.ru>
2019-07-25 02:54:00 +0300

    Delete legacy code

--- Source/shoot.h
+++ Source/shoot.h
@@ -35,15 +35,3 @@ typedef struct Coordinate {
 } Coordinate;
 
 void initCoordiante(Coordinate *coord, int x, int y);
-
-void choosing_comp_strategy(bool ship_comp_field[10][15]);
-
-void change_x_leftkey(int* x);
-void change_x_rightkey(int* x);
-void change_y_topkey(int* y);
-void change_y_botkey(int* y);
-
-bool check_border_left_shoot(int y, int x, int field[10][15]);
-bool check_border_right_shoot(int y, int x, int field[10][15]);
-bool check_border_bot_shoot(int y, int x, int field[10][15]);
-bool check_border_top_shoot(int y, int x, int field[10][15]);
